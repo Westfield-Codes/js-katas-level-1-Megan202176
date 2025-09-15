175ebf10c092aa8ed5cdff7a593a1d87be3b6917
Megan202176 <megan202176@student-schoolsofwestfield.org>
2025-09-15 12:20:55 -0400

    ....

--- done.js
+++ done.js
@@ -3,7 +3,10 @@
 /* COLOR KATAS */
 
 /* No Prompt */
-
+// make a string variable for a color, set it equal to your favorite
+var color = "green"
+// alert the message" "My favorite color is " plus the color
+alert ("My favorite color is" + color );
 
 /* Prompt */
 
